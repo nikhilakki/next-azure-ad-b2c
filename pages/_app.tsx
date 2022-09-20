ec92399087dc570d4c8b0030697a0a3796fdd409
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2022-09-21 00:05:27 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+  export default function App({
+    Component,
+    pageProps: { session, ...pageProps }}: AppProps,
+  ) {
+    return (
+        <Component {...pageProps} />
+    )
+  }
