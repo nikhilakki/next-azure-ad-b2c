7e7619299b9d5354083babcdaf79a5906f689c6e
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2022-09-21 00:25:12 +0530

    static export config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import { MsalProvider } from '@azure/msal-react';
+import { NextComponentType } from 'next';
+import { AppProps } from 'next/app';
 import { msalInstance } from '../services/msal';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MsalProvider instance={msalInstance}>
       <Component {...pageProps} />
--- services/msal.ts
+++ services/msal.ts
@@ -3,16 +3,19 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-import * as msal from "@azure/msal-browser";
+import { PublicClientApplication, Configuration } from "@azure/msal-browser";
 
-const msalConfig = {
+const clientId = process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID || ""
+const authority = `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID}`
+console.log({ clientId, authority })
+const msalConfig: Configuration = {
     auth: {
-        clientId: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID,
-        authority: `https://login.microsoftonline.com/${process.env.NEXT_PUBLIC_AZURE_AD_TENANT_ID}`,
+        clientId: clientId,
+        authority: authority,
         redirectUri: '/'
     }
-};
+}
 
-const msalInstance = new msal.PublicClientApplication(msalConfig);
+const msalInstance = new PublicClientApplication(msalConfig)
 
 export { msalInstance }
